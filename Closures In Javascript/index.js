f9774a9bbf82c89b8ce57a38f59b51e082fffd04
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-07-18 15:07:45 +0530

    Closures in Javscript

--- Closures In Javascript/index.js
+++ Closures In Javascript/index.js
@@ -1,1 +1,81 @@
 /* Closures in Javascript */
+
+// function x() {
+    
+//     var a = 7;
+//     function y(){
+//         console.log(a);
+//     }
+
+//     y();
+// }
+
+// x();
+
+
+// function x() {
+    
+//     var a = 7;
+//     var func = function y() {
+//         console.log(a);
+//     };
+
+//     func();
+
+// }
+
+// x();
+
+
+// function x(y) {
+//     var a = 7;
+//     y(a);
+// }
+
+// x(function y(a){
+//     console.log(a);
+// });
+
+
+/* we can assign a function to a variable */
+// function x(){
+
+//     let b = 12;
+//     var a = function y(){
+//         console.log(b);
+//     }
+
+//     a();
+// }
+
+// x();
+
+
+/* We can pass function as an argument / parameter to another function */
+// function x(callback){
+//     var a = 7;
+//     callback(a);
+// }
+
+// x(function y(a){
+//     console.log(a);
+// });
+
+
+/* We can even return these function from another function */
+// function x() {
+//     var a = 7;
+//     function y() {
+//         console.log(a);
+//     }
+
+//     return y;
+// }
+
+// var z = x();
+// console.log(z);
+// .............  1000s lines of code
+// z();
+
+ 
+
